0af66870bcdaf942cdbb28cae7aa5dceb3a946b2
denisOmega5566 <74858963+denisOmega5566@users.noreply.github.com>
2020-11-27 17:05:05 +0200

    Update Customer.java

--- src/домен/Customer.java
+++ src/домен/Customer.java
@@ -1,1 +1,61 @@
+public class CustomerTest
+{
+    public static void main(String args[])
+    {
+        Customer myCustomer=new Customer();
+        myCustomer.displayCustomerInfo();
+    }
+}
+public class Customer {
 
+    private int ID;
+    private boolean isNew;
+    private float total;
+
+    public int getID() {
+        return ID;
+    }
+    public boolean isIsNew() {
+        return isNew;
+    }
+    public float getTotal() {
+        return total;
+    }
+
+
+
+
+    public void setID(int ID) {
+       if(ID>0){
+       this.ID = ID;
+       }
+       else {
+         System.out.println("Error: ID 0> : " +ID+ "");
+       }
+    }
+    public void setIsNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+    public void setTotal(float total) {
+        if(total>0){
+       this.total = total;
+       }
+       else {
+         System.out.println("Error: total : 0 > " +total+ "");
+       }
+    }
+
+    public Customer(){
+    ID = 28;
+    isNew = true;
+    total = 228;
+    }
+
+
+    public void displayCustomerInfo() {
+
+    System.out.println("Клиент : " +ID);
+    System.out.println("Новый клиент : " + isNew);
+    System.out.println("Всего за год : " + total);
+} 
+}
